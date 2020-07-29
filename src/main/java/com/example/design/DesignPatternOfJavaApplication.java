a7229366a6f1c5dce5ade85a47c6faeef94d0971
jay.moon <jay.moon@kakaocorp.com>
2020-07-29 16:50:26 +0900

    Initial commit

---
+++ src/main/java/com/example/design/DesignPatternOfJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternOfJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternOfJavaApplication.class, args);
+    }
+
+}
